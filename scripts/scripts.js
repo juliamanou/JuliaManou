ad686d792f8b71f609a6e6f4123e68a8a5a42702
juliamanou <s_julia.manou@ousd.org>
2021-10-14 23:43:28 -0700

    bonked

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,8 @@
 $(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = 'views/' + $(this).data('include') + '.html'
-      $(this).load(file)
-    })
-
-    document.title = "Julia Manou"
-
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = 'views/' + $(this).data('include') + '.html'
+    $(this).load(file)
   })
+  document.title = "Julia Manou"
+})
